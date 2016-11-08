9b6ca0f4914f58c351fa97f4993caf80a49c2014
naganori <gabriel.r.duque@gmail.com>
2016-11-08 16:05:50 +0100

    Creating the structure for Sharpboy and the main objects

---
+++ sharpboy/sharpboy/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿module sharpboy.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly:AssemblyTitle("sharpboy")>]
+[<assembly:AssemblyDescription("")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("")>]
+[<assembly:AssemblyProduct("")>]
+[<assembly:AssemblyCopyright("naganori")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
---
+++ sharpboy/sharpboy/sharpboy.fs
@@ -0,0 +1,11 @@
+﻿open System
+
+//Eight 8-bit registers A,B,C,D,E,F,H,L each one represented by a byte
+let mutable registers = Array.create 8 0uy
+
+//Two 16-bit registers SP which points to the stack position and PC which points to the next instruction to be executed
+let mutable PC = 0us
+let mutable SP = 0us
+
+
+ 
