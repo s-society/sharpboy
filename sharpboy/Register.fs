f86e1dedc6898471f9d8d8ed6c893926fd4f5955
ilnoumri <ilnoumri@yahoo.fr>
2016-12-12 20:09:51 +0100

    Adding registers

--- sharpboy/Register.fs
+++ sharpboy/Register.fs
@@ -1,2 +1,4 @@
 ﻿module Register
 
+let mutable A, B, C, D, E, F, H, L, SP, PC = 0x01uy,0uy,0x13uy,0uy,0xD8uy,0xB0uy,0x01uy,0x4duy,0xFFFEus,0x0100us 
+let mutable ZF,NF,HF,CF = true, false, true, true
